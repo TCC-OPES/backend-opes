53ad4b1b9f3c45b11c41006120fd0b8d2899a33a
vitormsantanaa <vitormanoelssz1704@gmail.com>
2026-04-18 12:04:59 -0300

    FEAT: Criação de login de usuário

--- core/urls/usuario.py
+++ core/urls/usuario.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from core.views import CadastroView
+from core.views import CadastroView, LoginView, MeView
 
 urlpatterns = [
     path('cadastro/', CadastroView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('me/', MeView.as_view()),
 ]
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,1 +1,3 @@
 from .usuario import CadastroView
+from .auth import LoginView
+from .me import MeView
