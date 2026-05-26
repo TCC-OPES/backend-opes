e58608ab5fa83ccd95aaf070af83ebe279ba1ee1
vitoriadesouza <vitoriasouza.ifc@gmail.com>
2026-05-25 23:55:11 -0300

    FIX: Comecei a arrumar algumas coisas para o upload de imagem

--- core/urls/usuario.py
+++ core/urls/usuario.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from core.views import CadastroView, LoginView, MeView
+# Importações diretas e seguras para o Django não se perder
+from core.views.usuario import CadastroView, AtualizarFotoPerfilView
+from core.views import LoginView, MeView
 
 urlpatterns = [
+    # Rotas originais do seu sistema que fazem o login funcionar
     path('cadastro/', CadastroView.as_view()),
     path('login/', LoginView.as_view()),
     path('me/', MeView.as_view()),
+    
+    # Nova rota da foto de perfil
+    path('perfil/foto/', AtualizarFotoPerfilView.as_view()),
 ]
