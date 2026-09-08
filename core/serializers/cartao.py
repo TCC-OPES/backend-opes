2c8729452ea2786f9f431f37d77bd639a56162ec
vitoriadesouza <vitoriasouza.ifc@gmail.com>
2026-09-08 08:49:36 -0300

    FEAT: Alteração no recebimento dos dados dos Cartões

--- core/serializers/cartao.py
+++ core/serializers/cartao.py
@@ -4,6 +4,10 @@ from core.models import Cartao
 
 
 class CartaoSerializer(serializers.ModelSerializer):
+    vencimento = serializers.DateField(
+        input_formats=['%d/%m/%Y', '%Y-%m-%d'],
+        format='%d/%m/%Y'
+    )
 
     class Meta:
         model = Cartao
